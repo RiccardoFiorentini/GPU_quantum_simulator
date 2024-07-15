c5caaaf10036e305006655c45fd6c8f1eb9137c8
Hack-Rick <66008637+Hack-Rick@users.noreply.github.com>
2024-07-15 10:33:53 +0200

    Costant memory and debugging

--- tester.bash
+++ tester.bash
@@ -1,36 +1,47 @@
 #!/bin/bash
 
-# Define the number of times to run the C++ program
+# Define the number of times to run
 NUM_RUNS=5
 
-NUM_START=5
-NUM_END=22
-
 #Loop to run the C++ program multiple times
-for((j = 19; j <= 22; j++)); do
-    echo "Random num $j:" >>  OutputTest1.txt
+echo "C version:" >> OverallTest.csv
+for((j = 5; j <= 18; j++)); do
+    echo "Num QBit $j:" >>  OverallTest.csv
     for ((i = 1; i <= NUM_RUNS; i++)); do  
-        ./cudaExe2 "random_circs_ad/random_$j.qasm" >>   OutputTest1.txt
+        ./CExe "random_circs_ad/random_$j.qasm" >> OverallTest.csv
     done
-    echo "----------" >>  OutputTest1.txt
 done
 
-#echo "Cuda Without Preprocessing:" >>   OutputCUDAOld.txt
-#for((j = NUM_START; j <= NUM_END; j++)); do
-#    echo "Random num $j:" >> OutputCUDAOld.txt
-#    for ((i = 1; i <= NUM_RUNS; i++)); do
-#        #echo "Run $i:" >> results.txt   
-#        ./cudaExe1 "random_circs_ad/random_$j.qasm" >>  OutputCUDAOld.txt
-#    done
-#    echo "----------" >>  OutputCUDAOld.txt
-#done
-#
-#echo "Cpp Results" >>  OutputCPP.txt
-#for((j = NUM_START; j <= 14; j++)); do
-#    echo "Random num $j:" >> OutputCPP.txt
-#    for ((i = 1; i <= NUM_RUNS; i++)); do
-#        #echo "Run $i:" >> results.txt   
-#        ./CppExe "random_circs/random_$j.qasm" 1 >>  OutputCPP.txt
-#    done
-#    echo "----------" >>  OutputCPP.txt
-#done
+#Scegli numero di thread ottimale
+echo "Cuda Without Preprocessing:" >> OverallTest.csv
+for((j = 5; j <= 22; j++)); do
+    echo "Num QBit $j:" >> OverallTest.csv
+    for ((i = 1; i <= NUM_RUNS; i++)); do
+        ./cudaExe1 "random_circs_ad/random_$j.qasm" >>  OverallTest.csv
+    done
+done
+
+#Scegli numero di thread ottimale
+echo "Cuda With Preprocessing:" >> OverallTest.csv
+for((j = 5; j <= 22; j++)); do
+    echo "Num QBit $j:" >> OverallTest.csv
+    for ((i = 1; i <= NUM_RUNS; i++)); do
+        ./cudaExe2 "random_circs_ad/random_$j.qasm" >>  OverallTest.csv
+    done
+done
+
+echo "Cuda With Preprocessing merged matrix:" >> OverallTest.csv
+for((j = 5; j <= 22; j++)); do
+    echo "Num QBit $j:" >> OverallTest.csv
+    for ((i = 1; i <= NUM_RUNS; i++)); do
+        ./cudaExe3 "random_circs_ad/random_$j.qasm" >>  OverallTest.csv
+    done
+done
+
+echo "Cuda With Preprocessing read only:" >> OverallTest.csv
+for((j = 5; j <= 22; j++)); do
+    echo "Num QBit $j:" >> OverallTest.csv
+    for ((i = 1; i <= NUM_RUNS; i++)); do
+        ./cudaExe4 "random_circs_ad/random_$j.qasm" >>  OverallTest.csv
+    done
+done
